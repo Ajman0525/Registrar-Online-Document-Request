1143a6942f87c22c6aabefad6ae763fd2c01f081
NightsProject <nightnightproject@gmail.com>
2025-10-16 15:03:50 +0800

    ODR-1 fix function name from landing to request

--- app/user/request/controller.py
+++ app/user/request/controller.py
@@ -3,6 +3,6 @@ from flask import render_template, session, redirect, url_for
 
 
 @request_bp.route('/user/request')
-def landing():
+def request():
  
     return render_template('/user/request.html', username=session.get('username'), active='request')
