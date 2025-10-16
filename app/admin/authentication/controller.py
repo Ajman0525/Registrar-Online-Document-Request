99e011b062c5c6652399a2a6cc5c193e6eff0211
NightsProject <nightnightproject@gmail.com>
2025-10-16 16:01:05 +0800

    ODR-1 Added a working web server sample to all endpoints and installed flask-bootstrap5

--- app/admin/authentication/controller.py
+++ app/admin/authentication/controller.py
@@ -2,6 +2,14 @@ from . import authentication_admin_bp
 from flask import render_template, session, redirect, url_for
 
 
+#testing
+@authentication_admin_bp.route("/")
+def index():
+    return render_template('index.html')
+#testing
+
+
+
 @authentication_admin_bp.route('/admin/login')
 def admin_login():
  
