09b5f4444276d17314cb91ef7c0c92aeb2c90bbc
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-22 15:00:39 +0800

    feat(layout): update registrar master layout to properly reflect the static sidebar with working navlinks

---
+++ frontend/src/pages/layouts/RegistrarMasterLayout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../../components/admin/Sidebar";
+import "./RegistrarMasterLayout.css";
+
+function RegistrarMasterLayout(){
+    return (
+      <div className="registrar-master-layout">
+        <Sidebar />
+
+        <main className="content-area">
+          <Outlet />
+        </main>
+      </div>
+    );
+}
+
+export default RegistrarMasterLayout;
