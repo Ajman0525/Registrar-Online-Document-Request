af8fe8dfa7c448a3c889fb5a7d3c4a9ea6ecc110
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-10-22 13:29:39 +0800

    feat(ContentBox): Add ContentBox component with styling and integrate into UserMasterLayout

--- frontend/src/pages/layouts/UserMasterLayout.js
+++ frontend/src/pages/layouts/UserMasterLayout.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Header from "../../components/user/Header";
 import Footer from "../../components/user/Footer";
+import ContentBox from "../../components/user/ContentBox";
 import "./UserMasterLayout.css";
 
 function UserMasterLayout() {
     return (
         <div className="user-master-layout">
             <Header />
+
         <div className="content-area">
-            <p>Content will be rendered here based on user navigation</p>
+            <ContentBox>
+                <p>Content will be rendered here based on user navigation</p>
+            </ContentBox>
         </div>
+
             <Footer />
         </div>
     )
