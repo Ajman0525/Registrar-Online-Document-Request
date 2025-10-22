fa89f495ed36d4ad4acb105c7f8e629497219722
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-10-22 14:24:17 +0800

    feat(Request): Add Request component and integrate into user routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import UserMasterLayout from "./pages/layouts/UserMasterLayout";
 import UserLogin from "./pages/user/UserLogin";
 import Tracking from "./pages/user/Tracking";
 import DocumentList from "./pages/user/DocumentList";
+import Request from "./pages/user/Request";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/user/Landing" element={< Landing />} />
 
         <Route path="/user" element={<UserMasterLayout />}>
-          <Route path="UserLogin" element={< UserLogin />} />
+          <Route path="UserLogin" element={< UserLogin />}/>
+          <Route path="Request" element={< Request />} />
           <Route path="View" element={< DocumentList />} />
           <Route path="Track" element={< Tracking />} />
         </Route>
--- frontend/src/pages/user/UserLogin.js
+++ frontend/src/pages/user/UserLogin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./UserLogin.css";
 import ContentBox from "../../components/user/ContentBox";
 
@@ -7,6 +8,7 @@ function UserLogin() {
         <div className="Login-page">
             <ContentBox>
                 <p>User Login Page</p>
+                <Link to="/user/Request">Login</Link>
             </ContentBox>
         </div>
     );
