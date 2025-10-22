b0f876032ed7d451b7a91414f941bd52d81ab64a
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-10-22 15:03:19 +0800

    feat(admin): add a static sidebar on the admin page

---
+++ frontend/src/components/admin/Sidebar.jsx
@@ -0,0 +1,50 @@
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
+import DashboardIcon from '../icons/DashboardIcon';
+import DocumentsIcon from '../icons/DocumentsIcon';
+import LogsIcon from '../icons/LogsIcon';
+
+
+
+const navItems = [
+    {name: 'Dashboard', path: '/admin/dashboard', icon: DashboardIcon},
+    {name: 'Document', path: '/admin/document', icon: DocumentsIcon},
+    {name: 'Logs', path: '/admin/logs', icon: LogsIcon}
+]
+
+const Sidebar = () => {
+    return (
+        <aside className= "admin-sidebar">
+            <div className = "sidebar-header">
+            
+                    <img src="/assets/MSUIITLogo.png" alt="MSUIIT Logo" className="brand-logo" />
+                    <div className ="brand-name">
+                        <p id = "online">Online</p>
+                        <p>Document</p>
+                        <p>Request</p>
+                    </div>
+            
+                
+            </div>
+
+            <nav>
+                    {navItems.map((item) => (
+                        <NavLink
+                            key={item.name}
+                            to={item.path}
+
+                            className ={({isActive}) =>
+                                isActive ? 'nav-item active' : 'nav-item'
+                            }
+                        >
+                            <item.icon  className = "nav-icon" />
+                            {item.name}
+                        </NavLink>
+                ))}
+            </nav>
+        </aside>
+    )
+
+};
+
+export default Sidebar;
