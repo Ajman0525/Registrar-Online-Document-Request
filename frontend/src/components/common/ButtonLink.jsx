b2eed8dcf9112bc0ba4b0944314800eec40931c1
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-10-26 06:39:53 +0800

    ODR-3 feat(ButtonLink): enhance ButtonLink component with click handling and CSS adjustments

--- frontend/src/components/common/ButtonLink.jsx
+++ frontend/src/components/common/ButtonLink.jsx
@@ -1,13 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import "./ButtonLink.css";
 
-function ButtonLink({ to, placeholder, className }) {
+function ButtonLink({ to, placeholder, className, onClick }) {
     const navigate = useNavigate();
 
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        } else if (to) {
+            navigate(to);
+        }
+    };
+    
     return (
         <button
-            onClick = { () => navigate(to)}
-            className= {`button-link ${className? `${className}` : "" }`}
+            onClick={handleClick}
+            className={`button-link ${className ? `${className}` : ""}`}
         >
             {placeholder}
         </button>
