44e5c367ad2eac1e6c197b7afee44132739a9aba
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-11-02 16:45:06 +0800

    ODR-9 feat(admin): add a header on the dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { getCSRFToken } from "../../utils/csrf";
 import { useNavigate } from "react-router-dom";
+import './Dashboard.css';
 
-function Dashboard(){
-  return(
-      <div>
-        <h1 classname = "header">In Progress</h1>
+function Dashboard() {
+  return (
+    // The main content area wrapper
+    <div >
+
+      {/* --- FOCUS: THE DASHBOARD HEADER --- */}
+      <div className="header-simple">
+        <h1>Dashboard</h1>
       </div>
+      {/* --- END DASHBOARD HEADER --- */}
+
+
+    </div>
   );
 }
 
