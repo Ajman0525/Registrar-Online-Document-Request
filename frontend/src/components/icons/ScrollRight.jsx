ed04646949bd7854e04c2d87c7922704b0ef2ecc
Ajman <ajman.mocsana@g.msuiit.edu.ph>
2025-11-05 21:42:47 +0800

    ODR-9 chore: add a scrolling icon for the stat cards

---
+++ frontend/src/components/icons/ScrollRight.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ScrollRight = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" {...props}>
+        <path d="M9.5 18.25L15.75 12L9.5 5.75" 
+            stroke="black" 
+            stroke-width="1.5" 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+        />
+    </svg>
+);
+
+export default ScrollRight;
