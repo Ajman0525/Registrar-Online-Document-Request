f69d89307478bb072fbd873d8e6130dae504df2e
NightsProject <nightnightproject@gmail.com>
2025-11-05 19:48:50 +0800

    implementing the request page document list

--- app/user/request/controller.py
+++ app/user/request/controller.py
@@ -2,6 +2,8 @@ from . import request_bp
 from flask import render_template, session, redirect, url_for, jsonify, g
 from app.utils.decorator import jwt_required_with_role
 from flask_jwt_extended import get_jwt_identity
+from models import get_student_data, generate_unique_request_id, store_request, store_student_info
+from app.user.document_list.models import DocumentList
 
 role = 'user'
 
@@ -10,28 +12,37 @@ role = 'user'
 def get_request_page_data():
     """
     Step 1: Initialize the db with the student_id and request_id 
-    Step 2: Get student data from external DB then fill in the database
+            and Get student data from external DB then fill in the database
     Step 2: Get all available documents for request
     Step 3: Return JSON data to React
     """
     try:
         # step 1: initialize the db
-        request_id = generated_id
+        request_id = generate_unique_request_id()
         student_id = get_jwt_identity()
         
         ##store req_id and student_id to db
+        store_request(request_id, student_id)
+        
         
         #Fetch student info
         student_data = get_student_data(student_id)
 
+        student_name = student_data.get("full_name")
+        student_contact = student_data.get("contact_number")
+        student_email = student_data.get("email")
+        
+        #Store student info in the database
+        store_student_info(student_id, student_name, student_contact, student_email)
 
         # Step 2: Fetch documents available for request
-        documents = get_all_documents()
+        documents = DocumentList.get_all_documents()
 
-        # Step 3: Combine results
+        # Step 3: send the needed data to React
         return jsonify({
             "status": "success",
-            "student": student_data,
+            "request_id": request_id,
+            "student_name": student_name,
             "documents": documents
         })
 
