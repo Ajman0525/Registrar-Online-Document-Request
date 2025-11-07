31085fc907bf4c2a3350d1931f620c3f427b2ca6
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-11-07 14:43:28 +0800

    Merge branch 'ODR-5-user---request-page' into feature/ODR-3-user-authentication

--- app/user/document_list/controller.py
+++ app/user/document_list/controller.py
@@ -2,7 +2,7 @@ from . import document_list_bp
 from flask import render_template, session, redirect, url_for
 
 
-@document_list_bp.route('/user/document_list')
+@document_list_bp.route('/user/document-list')
 def document_list():
  
     return render_template('/user/document_list.html', username=session.get('username'), active='document_list')
