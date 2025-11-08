a9d8b1e62c33bcef3689b1480999a54a1fe11c43
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-11-08 22:21:29 +0800

    ODR-10 feat(FileCard): add FileCard component with styling

---
+++ frontend/src/components/common/FileCard.jsx
@@ -0,0 +1,11 @@
+import "./FileCard.css";
+
+function FileCard({ fileName, requirements, onClick }) {
+    return (
+        <div className="file-card">
+            <h3 className="file-name">Hello File</h3>
+        </div>
+    );
+}
+
+export default FileCard;
