290ca7ac0cc3262168fdded33ed35428de5cf8e9
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-11-10 02:29:54 +0800

    ODR-10 feat(AddCard): make card-container clickable by adding onClick handler

--- frontend/src/components/common/AddCard.jsx
+++ frontend/src/components/common/AddCard.jsx
@@ -2,7 +2,7 @@ import "./FileCard.css";
 
 function AddCard({onClick }) {
     return (
-        <div className="card-container">
+        <div className="card-container" onClick={onClick}>
             <div className="add-card">
             <img src="/assets/PlusIcon.svg" alt="plus icon" className="plus-icon" />
             </div>
