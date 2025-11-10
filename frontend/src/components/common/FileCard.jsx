7bbc0139a154516504c8d8ac22c7733e0d950d25
BolandoKen <janatkinson.bolando@g.msuiit.edu.ph>
2025-11-10 16:43:32 +0800

    ODR-10 feat(FileCard): enhance file card component with document details, pricing, and action buttons

--- frontend/src/components/common/FileCard.jsx
+++ frontend/src/components/common/FileCard.jsx
@@ -1,9 +1,42 @@
 import "./FileCard.css";
+import ButtonLink from "./ButtonLink";
 
-function FileCard({ fileName, requirements, onClick }) {
+function FileCard({ documentName, docDescription, requirements = [], cost, onClick }) {
     return (
-        <div className="file-card">
-            <h3 className="file-name">Hello File</h3>
+        <div className="card-container">
+             <div className="file-card">
+                <div className="logo-section">
+
+                </div>
+
+                <div className="name-and-description-section">
+                    <h2 className="document-name">{documentName}</h2>
+                    <p className="subtext">{docDescription}</p>
+                </div>
+
+                <div className="requirements-section">
+                    <p className="requirements-title">Requirements:</p>
+                    <ul className="subtext">
+                        {requirements.map((req, index) => (
+                        <li key={index}>{req}</li>
+                        ))}
+                    </ul>
+                </div>
+
+
+                <div className="price-and-action-section">
+                    <div className="card-price-section">
+                        <p className="price-amount">Php{Number(cost).toFixed(2)}</p>
+                        <p className="card-price-text">per copy</p>
+                    </div>
+                    <div className="action-section">
+                        <ButtonLink text="Edit" placeholder={"Edit"} onClick={onClick} />
+                        <button className="delete-button">
+                            <img src="/assets/TrashWhite.svg" alt="Delete" />
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
