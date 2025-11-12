0819323aaf7c8fefe8b0fbc2414040804a3ff1bc
Ajman <mocsanaajman@gmail.com>
2025-11-12 22:02:12 +0800

    ODR-8 style: place the secure authentication description below the sign in button

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -112,20 +112,19 @@ function AdminLogin() {
                     )}
                   </div>
 
+                  <div className="pt-6 flex justify-center border-t border-gray-200">
+                    <div className="flex items-center gap-2 text-sm text-gray-600">
+                      <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                      </svg>
+                      <span>Secure authentication with Google</span>
+                    </div>
+                  </div>
+
                   <div className="text-center">
                     <p className="text-xs text-gray-500">
                       By signing in, you agree to our Terms of Service and Privacy Policy
                     </p>
-                  </div>
-                </div>
-
-                {/* Additional info */}
-                <div className="pt-6 border-t border-gray-200">
-                  <div className="flex items-center gap-2 text-sm text-gray-600">
-                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                    </svg>
-                    <span>Secure authentication with Google</span>
                   </div>
                 </div>
               </div>
