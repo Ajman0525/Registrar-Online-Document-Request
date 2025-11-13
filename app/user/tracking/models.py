24693f4eb8ce580464da7c56081796b29c491f66
mib <karol.tacangg@gmail.com>
2025-11-13 14:29:07 +0800

    ODR-4 add payment status to query

--- app/user/tracking/models.py
+++ app/user/tracking/models.py
@@ -20,7 +20,7 @@ class Tracking:
         try:
             # Query to get the main request details
             cur.execute("""
-                SELECT status, total_cost, contact_number, payment_status
+                SELECT status, total_cost, contact_number
                 FROM requests
                 WHERE request_id = %s AND student_id = %s
             """, (tracking_number, student_id))
