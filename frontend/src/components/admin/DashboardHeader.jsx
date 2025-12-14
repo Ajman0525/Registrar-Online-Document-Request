06a02f0eaaf7cfead6bdf853f0201c5089672daa
Ajman <mocsanaajman@gmail.com>
2025-12-14 22:48:47 +0800

    ODR-47 chore: import the logout icon inside the dashboard header react component

--- frontend/src/components/admin/DashboardHeader.jsx
+++ frontend/src/components/admin/DashboardHeader.jsx
@@ -2,4 +2,5 @@ import React, { useState, useRef, useEffect } from "react";
 import SettingsIcon from "../components/SettingsIcon";
 import NotificationIcon from "../components/NotificationIcon";
 import SearchIcon from "../components/SearchIcon";
+import LogoutIcon from "../components/LogoutIcon";
 import "../../pages/admin/Dashboard.css";
